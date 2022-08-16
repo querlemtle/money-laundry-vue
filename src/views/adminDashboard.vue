56326206bad328947255ab5f01279531c2024062
kuomin <tw.kuomin@gmail.com>
2022-08-16 21:28:30 +0800

    Build adminDashboard.vue

---
+++ src/views/adminDashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header />
+    <Tabs />
+    <DataList />
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import DataList from "../components/DataList.vue";
+import Tabs from "../components/Tabs.vue";
+
+export default {
+  components: {
+    Header,
+    DataList,
+    Tabs,
+  },
+};
+</script>
